c14e2ccf0a025c5088e8b0516c3c8d66af7b1a47
lidong <zhubaiying@126.com>
2012-08-15 14:59:10 +0800

    添加昵称

--- socket.js
+++ socket.js
@@ -1,11 +1,19 @@
 //var io = require('/usr/local/node/npm/node_modules/socket.io').listen(8001);
 var io = require('socket.io').listen(8001);
 io.sockets.on('connection', function(socket){
-    var get;
+    var get,
+        nickname;
+    socket.on('nickname', function(name){
+        socket.set('nickname', name, function(){
+            io.sockets.emit('ready');  
+        });  
+    });
     socket.on('send', function(data){
-        get = data.my;
-        socket.broadcast.emit('user connected');
-        io.sockets.emit('all',{data : get});
+        socket.get('nickname',function(err, name){
+            get = data.my;
+            socket.broadcast.emit('user connected');
+            io.sockets.emit('all',{data : get, name : name.name});
+        });
     });
    
 });
