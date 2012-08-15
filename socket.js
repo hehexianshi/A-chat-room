7f28e85263434b5eef381478c4b2289a492e51f6
lidong <zhubaiying@126.com>
2012-08-15 15:52:42 +0800

    添加离开提示

--- socket.js
+++ socket.js
@@ -1,23 +1,26 @@
-//var io = require('/usr/local/node/npm/node_modules/socket.io').listen(8001);
-var io = require('socket.io').listen(8001);
+var io = require('/usr/local/node/npm/node_modules/socket.io').listen(8001);
+//var io = require('socket.io').listen(8001);
 io.sockets.on('connection', function(socket){
+
+    socket.broadcast.emit('connected', {data : 'new user insert', name : '管理员'});
     var get,
         nickname;
     socket.on('nickname', function(name){
         socket.set('nickname', name, function(){
-            io.sockets.emit('ready');  
+            //io.sockets.emit('ready');  
         });  
     });
     socket.on('send', function(data){
         socket.get('nickname',function(err, name){
             get = data.my;
-            socket.broadcast.emit('user connected');
             io.sockets.emit('all',{data : get, name : name.name});
         });
     });
+    socket.on('disconnect', function(){
+        socket.get('nickname', function(err, name){
+            
+            io.sockets.emit('disconnection', {data : name.name});
+        });
+    });
    
-});
-io.sockets.on('disconnect', function(socket){
-    io.sockets.emit('all', {data : 'user by'});      
-        
 });
